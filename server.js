8eeae8b1b9ceab4bc8889c51414d7b257297c3d5
AishwaryaSrinath18 <141122047+AishwaryaSrinath18@users.noreply.github.com>
2024-10-21 10:32:07 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { ObjectId } = require('mongodb');  // Import ObjectId
 
 const app = express();
 
